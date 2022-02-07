4d211a56096230da646c92192a90b4447d3f0956
layan2k <lesliedeeshumba@gmail.com>
2022-02-07 14:10:18 +0200

    fix6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,12 @@ Configure Flask Routes using APPviews
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -22,12 +28,10 @@ def stats():
     returns stats count and returns it
     as JSON
     """
-    counts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(counts)
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+    return jsonify(classes)
