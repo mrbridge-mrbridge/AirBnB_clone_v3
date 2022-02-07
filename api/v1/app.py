816f4f4b1e6ab79018e61cac619c8ca9b10faf2e
layan2k <lesliedeeshumba@gmail.com>
2022-02-07 13:18:12 +0200

    app update

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,10 @@ Creates first Endpoint for our API
 """
 
 from models import storage
-from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
-import os
+from os import environ
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,24 +15,23 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def tearDown():
-    """Closes each SQLAlchemyTask"""
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found():
+def not_found(error):
     """Error 404"""
-    make_response(jsonify({'error': "Not found"}), 404)
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host_origin = os.getenv('HBNB_API_HOST')
-    port_origin = os.getenv('HBNB_API_PORT')
-    if not host_origin:
-        host_origin = '0.0.0.0'
-    if not port_origin:
-        port_origin = 5000
-    app.run(host=host_origin,
-            port=port_origin,
-            threaded=True)
+    """ Main Function """
+    host_orgin = environ.get('HBNB_API_HOST')
+    port_orgin = environ.get('HBNB_API_PORT')
+    if not host_orgin:
+        host = '0.0.0.0'
+    if not port_orgin:
+        port = '5000'
+    app.run(host=host_orgin, port=port_orgin, threaded=True)
